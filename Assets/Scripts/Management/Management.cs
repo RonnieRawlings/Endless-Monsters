20d4bd2bb037017e76ddf7d701a2c7b32e44f1a6
Ronnie Rawlings <rr258736@falmouth.ac.uk>
2023-09-04 17:17:39 +0100

    Imported/Implemented - TMP Essentials + Start Menu
    This commit imports the TMP Essentials package so TextMeshPro UGUI can be used and accessed in scripts. The game's start menu has also been added with working buttons for New Game, Load Game, Options, and Quit.
    
    Next, have the player appear on screen when new game is clicked.

---
+++ Assets/Scripts/Management/Management.cs
@@ -0,0 +1,20 @@
+// Author - Ronnie Rawlings.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Management : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Management/SceneOptions.cs
@@ -0,0 +1,14 @@
+// Author - Ronnie Rawlings.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneOptions : MonoBehaviour
+{
+    /// <summary> method <c>QuitApplication</c> Quits out of the application on button press. </summary>
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+}
