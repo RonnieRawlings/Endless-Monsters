bebf157948c8a19d48e81fe62773db4e89a8a8ed
Ronnie Rawlings <rr258736@falmouth.ac.uk>
2023-09-16 14:13:03 +0100

    Implemented - Damage Number Animation
    This commit adds a simple animation to the damage numbers using a coroutine with a quadratic curve.
    
    Next, improve the look of the damage numbers.

--- Assets/Scripts/Management/Battle Management/MoveDamageNums.cs
+++ Assets/Scripts/Management/Battle Management/MoveDamageNums.cs
@@ -6,16 +6,42 @@ using UnityEngine;
 
 public class MoveDamageNums : MonoBehaviour
 {
+    // Animation duration + final position.
+    public float duration = 1.0f;
+    public Vector3 offset = new Vector3(-130, 80, 0);
+
+    // Objs start position & exact time before routine begins.
+    private Vector3 startPosition;
+    private float startTime;
+
     /// <summary> interface <c>AnimateDamageNumObject</c> executes a simple animation for the damage nums before destorying them. </summary>
     public IEnumerator AnimateDamageNumObject()
     {
-        yield return new WaitForSeconds(0.5f);
+        // Animates the obj.
+        while (Time.time - startTime < duration)
+        {
+            // Animation elapsed time.
+            float t = (Time.time - startTime) / duration;
+
+            // Moves obj, t * t = quadratic curve.
+            transform.position = Vector3.Lerp(startPosition, startPosition + offset, t * t);
+
+            // Satisfies return req, waits no time.
+            yield return null;
+        }
+
+        // Removes damage num from scene.
         Destroy(this.gameObject);
     }
 
     // Called once on script initlization.
     private void OnEnable()
     {
+        // Set start pos + time.
+        startPosition = transform.position;
+        startTime = Time.time;
+
+        // Start the animation.
         StartCoroutine(AnimateDamageNumObject());
     }
 }
